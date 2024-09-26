51eb744cda14f3d3fad9847b9640fa94c9dad2d6
najwasazaaa <najwashafira41@gmail.com>
2024-09-26 19:40:24 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,10 @@
+print ('Program Menghitung Luas & keliling segitiga')
+print('-----------------------------------------')
+
+a = float(input('Masukan nilai alas :'))
+t = float(input('Masukkan nilai Tinggi :'))
+
+luas = 0.5*a*t
+keliling = a+a+a
+print('\nLuasnya =', str(luas))
+print('Kelilingnya =', str(keliling))
